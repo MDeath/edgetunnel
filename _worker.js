9165c9313a550aeed30d7c9b1c76526ee98822e9
M_Die <42289879+MDeath@users.noreply.github.com>
2025-06-12 11:07:45 +0800

    Update _worker.js
    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1612,7 +1612,6 @@ async function 生成配置信息(userID, hostName, sub, UA, RproxyIP, _url, fak
             ${clash}<br>
             ---------------------------------------------------------------<br>
             ################################################################<br>
-            ${cmad}
             `;
         return `<div style="font-size:13px;">${节点配置页}</div>`;
     } else {
@@ -2221,7 +2220,6 @@ async function KV(request, env, txt = 'ADD.txt') {
                     </div>
                     <br>
                     ################################################################<br>
-                    ${cmad}
                     ` : '<p>未绑定KV空间</p>'}
                 </div>
         
